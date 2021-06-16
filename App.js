fd45c880399bda1c6c88ba8a96e1443b7d7e520c
abhinav80066 <abhinav.chaudhary800@gmail.com>
2021-06-16 14:29:53 +0530

    completed the UI part

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import classes from './App.module.css';
 import ProductData from './ProductData';
 
 function App(){
+  const curentHour= new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +15,16 @@ function App(){
       </header>
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
-          <img src="https://imgur.com/iOeUBV7.png" alt="product review"/>
+          <img src="https://imgur.com/iOeUBV7.png" alt="product Preview"/>
+
+          {/* <div className={classes.TimeSection}>
+            <p>{`${curentHour}:${currentMinute}`} </p>
+          </div> */}
+
+          <div className={classes.HeartBeatSection}>
+            <i class="fas fa-heartbeat"></i>
+            <p>78 </p>
+          </div>
         </div>
         <div className={classes.ProductData}>
           <h1 className={classes.ProductTitle}>{ProductData.title}</h1>

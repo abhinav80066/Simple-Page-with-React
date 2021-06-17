b2f5b214fddc53aef8bd1522b04535939aaee560
abhinav80066 <abhinav.chaudhary800@gmail.com>
2021-06-17 17:39:44 +0530

    divided whole page into Components

--- App.js
+++ App.js
@@ -1,51 +1,21 @@
 import logo from './logo.svg';
 import classes from './App.module.css';
 import ProductData from './ProductData';
-
+import ProductPreview from './ProductPreview';
+import ProductDetails from './ProductDetails';
+import Topbar from './Topbar';
 function App(){
-  const curentHour= new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
-  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <nav className={classes.Topbar}>
-          <img src =" " alt="Amazon logo"></img> 
-        </nav>
-
-      </header>
+     <Topbar />
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
-          <img src="https://imgur.com/iOeUBV7.png" alt="product Preview"/>
-
-          {/* <div className={classes.TimeSection}>
-            <p>{`${curentHour}:${currentMinute}`} </p>
-          </div> */}
-
-          <div className={classes.HeartBeatSection}>
-            <i class="fas fa-heartbeat"></i>
-            <p>78 </p>
-          </div>
+          <ProductPreview />
         </div>
-        <div className={classes.ProductData}>
-          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-          <p className={classes.ProductDescription}>{ProductData.description}</p>
-
-          <h3 className={classes.SectionHeading}> Select Color</h3>
-          <div>
-            <img className={[classes.ProductImage, classes.SelectProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black Colored Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Colored Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colored Watch" />
-            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colored Watch" />
-          </div>
-
-          <h3 className={classes.SectionHeading}> Features</h3>
-          <div>
-            <button className={[classes.FeatureItem, classes.SelectFeatureItem].join(' ')}>Time</button>
-            <button className={classes.FeatureItem}>Heart Rate</button>
-          </div>
-
-          <button className={classes.PrimaryButton}>Buy Now</button>
-        </div>
+      <div className={classes.ProductData}>
+          <ProductDetails />
+      </div>
       </div>
     </div>
   );
---
+++ Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Topbar.module.css';
+
+const Topbar = () => {
+    return(
+        <header className="App-header">
+        <nav className={classes.Topbar}>
+          <img src =" " alt="Amazon logo"></img> 
+        </nav>
+        </header>
+    )
+}
+
+export default Topbar;
